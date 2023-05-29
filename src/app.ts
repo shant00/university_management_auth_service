6e0767d41f3e13914f628f593ea4401d628e4ff0
shant00 <sshanto.cse@gmail.com>
2023-05-30 01:45:12 +0600

    added eslint pritter

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("API is running");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running')
+})
 
-export default app;
+/**
+ NODE_ENV=development
+PORT=8000
+DATABASE_URL=mongodb+srv://university_mangement:LUrZcjD9LeajLEwW@cluster0.med0q.mongodb.net/univery-management?retryWrites=true&w=majority
+ */
+export default app
